a9df9658d99fc2574799f705d2057a648884f685
Hamelin Arthur <arthuros222@gmail.com>
2024-05-24 10:54:57 +0200

    refactor(game): refactor les collision

--- core/src/fr/arks/exiledarkanoid/gamephysics/Ball.java
+++ core/src/fr/arks/exiledarkanoid/gamephysics/Ball.java
@@ -4,10 +4,15 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Vector2;
+import fr.arks.exiledarkanoid.gamephysics.abstracts.Element;
 import fr.arks.exiledarkanoid.gamephysics.abstracts.MovingElement;
 import fr.arks.exiledarkanoid.gamephysics.bases.Position;
 import fr.arks.exiledarkanoid.gamephysics.bases.Size;
 import fr.arks.exiledarkanoid.gamephysics.bases.Speed;
+
+import java.util.Iterator;
 
 public class Ball extends MovingElement {
 
@@ -20,15 +25,92 @@ public class Ball extends MovingElement {
         ball_image = new Texture(Gdx.files.internal("ball.png"));
     }
 
-    public void move(Size mapSize) {
+    public void move() {
+        this.position.x += (int) (this.speed.vx * Gdx.graphics.getDeltaTime());
+        this.position.y += (int) (this.speed.vy * Gdx.graphics.getDeltaTime());
+    }
+
+    public void collideWith(BrickMap brickMap) {
+        Iterator<Brick> iter = brickMap.getBricks().iterator();
+        Vector2 ball_position = new Vector2(this.position.x, this.position.y);
+        while (iter.hasNext()) {
+            Brick brick = iter.next();
+            if (Intersector.overlaps(this.shape, brick.shape)) {
+                boolean ball_direction_x = this.speed.vx > 0;
+                boolean ball_direction_y = this.speed.vy > 0;
+                if (ball_direction_x && ball_direction_y) {
+                    Vector2 A = new Vector2(brick.position.x, brick.position.y);
+                    Vector2 B = new Vector2(brick.position.x + brick.size.height, brick.position.y + brick.size.height);
+                    this.bound(A, B, ball_position, true);
+                } else if (ball_direction_x) {
+                    Vector2 A = new Vector2(brick.position.x, brick.position.y + brick.size.height);
+                    Vector2 B = new Vector2(brick.position.x + brick.size.height, brick.position.y);
+                    this.bound(A, B, ball_position, false);
+                } else if (ball_direction_y) {
+                    Vector2 A = new Vector2(brick.position.x + brick.size.width - brick.size.height, brick.position.y + brick.size.height);
+                    Vector2 B = new Vector2(brick.position.x + brick.size.width, brick.position.y);
+                    this.bound(A, B, ball_position, true);
+                } else {
+                    Vector2 A = new Vector2(brick.position.x + brick.size.width - brick.size.height, brick.position.y);
+                    Vector2 B = new Vector2(brick.position.x + brick.size.width, brick.position.y + brick.size.height);
+                    this.bound(A, B, ball_position, false);
+                }
+                brickMap.removeBrick(brick);
+                break;
+            }
+        }
+    }
+
+    public void collideWith(Platform platform) {
+        if (this.position.x >= platform.position.x - this.size.width / 2 && this.position.x <= platform.position.x + platform.size.width && this.position.y <= platform.position.y + platform.size.height) {
+            this.speed.vy = - this.speed.vy + 10;
+            this.speed.vx += (int) (100 * (((float) (this.position.x - platform.position.x) / (float) platform.size.width) - 0.5));
+        }
+    }
+
+    public void collideWith(Size mapSize) {
         if (this.position.x >= mapSize.width - this.size.width || this.position.x <= 0) {
             this.speed.vx = -this.speed.vx;
         }
         if (this.position.y >= mapSize.height - this.size.width) {
             this.speed.vy = -this.speed.vy;
         }
-        this.position.x += (int) (this.speed.vx * Gdx.graphics.getDeltaTime());
-        this.position.y += (int) (this.speed.vy * Gdx.graphics.getDeltaTime());
+    }
+
+    private void bound(Vector2 A, Vector2 B, Vector2 ball_position, boolean ball_direction_y) {
+        int position_relative = positionRelative(A, B, ball_position);
+        if (position_relative == 0) {
+            this.speed.vx = -this.speed.vx;
+            this.speed.vy = -this.speed.vy;
+        } else {
+            if (ball_direction_y) {
+                if (position_relative == 1) {
+                    this.speed.vx = -this.speed.vx;
+                } else {
+                    this.speed.vy = -this.speed.vy;
+                }
+            } else {
+                if (position_relative == 1) {
+                    this.speed.vy = -this.speed.vy;
+                } else {
+                    this.speed.vx = -this.speed.vx;
+                }
+            }
+        }
+    }
+
+    private static int positionRelative(Vector2 A, Vector2 B, Vector2 C) {
+        /*
+         A function to know if the point C is under or above the line AB
+         */
+        float crossProduct = (B.x - A.x) * (C.y - A.y) - (B.y - A.y) * (C.x - A.x);
+        if (crossProduct > 0) {
+            return 1;
+        } else if (crossProduct < 0) {
+            return -1;
+        } else {
+            return 0;
+        }
     }
 
     @Override
--- core/src/fr/arks/exiledarkanoid/gamephysics/Playfield.java
+++ core/src/fr/arks/exiledarkanoid/gamephysics/Playfield.java
@@ -32,39 +32,14 @@ public class Playfield {
     }
 
     public void update() {
-        ball.move(this.size);
-        Iterator<Brick> iter = brickMap.getBricks().iterator();
-        Vector2 ball_position = new Vector2(ball.position.x, ball.position.y);
-        while (iter.hasNext()) {
-            Brick brick = iter.next();
-            if (Intersector.overlaps(ball.shape, brick.shape)) {
-                boolean ball_direction_x = ball.speed.vx > 0;
-                boolean ball_direction_y = ball.speed.vy > 0;
-                if (ball_direction_x && ball_direction_y) {
-                    Vector2 A = new Vector2(brick.position.x, brick.position.y);
-                    Vector2 B = new Vector2(brick.position.x + brick.size.height, brick.position.y + brick.size.height);
-                    this.bound(A, B, ball_position, true, ball);
-                } else if (ball_direction_x) {
-                    Vector2 A = new Vector2(brick.position.x, brick.position.y + brick.size.height);
-                    Vector2 B = new Vector2(brick.position.x + brick.size.height, brick.position.y);
-                    this.bound(A, B, ball_position, false, ball);
-                } else if (ball_direction_y) {
-                    Vector2 A = new Vector2(brick.position.x + brick.size.width - brick.size.height, brick.position.y + brick.size.height);
-                    Vector2 B = new Vector2(brick.position.x + brick.size.width, brick.position.y);
-                    this.bound(A, B, ball_position, true, ball);
-                } else {
-                    Vector2 A = new Vector2(brick.position.x + brick.size.width - brick.size.height, brick.position.y);
-                    Vector2 B = new Vector2(brick.position.x + brick.size.width, brick.position.y + brick.size.height);
-                    this.bound(A, B, ball_position, false, ball);
-                }
-                brickMap.removeBrick(brick);
-                break;
-            }
+
+        if (ball.position.y < platform.position.y + platform.size.height + 100) {
+            ball.collideWith(platform);
         }
-        if (ball.position.x >= platform.position.x - ball.size.width / 2 && ball.position.x <= platform.position.x + platform.size.width && ball.position.y <= platform.position.y + platform.size.height) {
-            ball.speed.vy = -ball.speed.vy + 10;
-            ball.speed.vx += (int) (100 * (((float) (ball.position.x - platform.position.x) / (float) platform.size.width) - 0.5));
-        }
+
+        ball.collideWith(brickMap);
+        ball.collideWith(size);
+
         if (Gdx.input.isTouched()) {
             Vector3 touchPos = new Vector3();
             touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
@@ -72,6 +47,7 @@ public class Playfield {
                 platform.position.x = (int) (touchPos.x - platform.size.width / 2);
             }
         }
+        ball.move();
     }
 
     public void reset() {
@@ -81,42 +57,6 @@ public class Playfield {
 
     public boolean isLost() {
         return ball.position.y < platform.position.y;
-    }
-
-    private static int positionRelative(Vector2 A, Vector2 B, Vector2 C) {
-        /*
-         A function to know if the point C is under or above the line AB
-         */
-        float crossProduct = (B.x - A.x) * (C.y - A.y) - (B.y - A.y) * (C.x - A.x);
-        if (crossProduct > 0) {
-            return 1;
-        } else if (crossProduct < 0) {
-            return -1;
-        } else {
-            return 0;
-        }
-    }
-
-    private void bound(Vector2 A, Vector2 B, Vector2 ball_position, boolean ball_direction_y, Ball ball) {
-        int position_relative = positionRelative(A, B, ball_position);
-        if (position_relative == 0) {
-            ball.speed.vx = -ball.speed.vx;
-            ball.speed.vy = -ball.speed.vy;
-        } else {
-            if (ball_direction_y) {
-                if (position_relative == 1) {
-                    ball.speed.vx = -ball.speed.vx;
-                } else {
-                    ball.speed.vy = -ball.speed.vy;
-                }
-            } else {
-                if (position_relative == 1) {
-                    ball.speed.vy = -ball.speed.vy;
-                } else {
-                    ball.speed.vx = -ball.speed.vx;
-                }
-            }
-        }
     }
 
     public void render(SpriteBatch batch) {
