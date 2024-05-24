807164ef6d168e35e3af7733a4f23796832e8d2e
hadri <hadrienchanvillard@gmail.com>
2024-05-24 11:37:04 +0200

    [ADD_BALL] feat(game): new images

--- core/src/fr/arks/exiledarkanoid/gamephysics/Platform.java
+++ core/src/fr/arks/exiledarkanoid/gamephysics/Platform.java
@@ -1,5 +1,4 @@
 package fr.arks.exiledarkanoid.gamephysics;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -18,6 +17,14 @@ public class Platform extends MovingElement {
     public Platform(Position position, Size size, Speed speed) {
         super(position, size, speed);
         platform_image = new Texture(Gdx.files.internal("paddle.png"));
+    }
+
+    public void update(int gameWidth) {
+        if (Gdx.input.isTouched()) {
+            if (Gdx.input.getX() >= (float) this.size.width / 2.0 && Gdx.input.getX() <= gameWidth - (float) this.size.width / 2.0) {
+                this.position.x = Gdx.input.getX() - this.size.width / 2;
+            }
+        }
     }
 
     @Override
